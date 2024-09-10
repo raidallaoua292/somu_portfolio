41174a41f237e161561445f8e718e85dd695f593
raidallaoua292 <raidallaoua292@gmail.com>
2024-09-10 07:59:25 +0100

    chore: Update npm dependency for @vercel/speed-insights to version 1.0.12

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const expoArabic = localFont({
   src: "./fonts/ExpoArabic-Regular.woff",
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         <Header/>
         {children}
+        <SpeedInsights/>
       </body>
     </html>
   );
